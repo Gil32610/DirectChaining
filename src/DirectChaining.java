c8e02b595774532ae9562216a042b370909bc242
GIL <gil.00000032610@unicap.br>
2023-05-27 10:05:24 -0300

    Mod hash function String

--- src/DirectChaining.java
+++ src/DirectChaining.java
@@ -4,5 +4,18 @@ public class DirectChaining {
     private LinkedList<String>[] hashTable;
     private final int maxChainSize = 5;
 
+    public DirectChaining(int size) {
+        hashTable = new LinkedList[size];
+    }
 
+    public int modASCIIHashFunction(String word, int M) {
+        char ch[];
+        ch = word.toCharArray();
+
+        int i, sum;
+        for (sum = 0, i = 0; i < word.length(); i++) {
+            sum += ch[i];
+        }
+        return sum % M;
+    }
 }
