927d4768bd887bd9b1010f526103dfad1f789701
GIL <gil.00000032610@unicap.br>
2023-05-27 11:00:01 -0300

    Delete method

--- src/DirectChaining.java
+++ src/DirectChaining.java
@@ -4,8 +4,11 @@ public class DirectChaining {
     private LinkedList<String>[] hashTable;
     private final int maxChainSize = 5;
 
+    private int size;
+
     public DirectChaining(int size) {
         hashTable = new LinkedList[size];
+        this.size = size;
     }
 
     public int modASCIIHashFunction(String word, int M) {
@@ -28,14 +31,32 @@ public class DirectChaining {
         } else hashTable[newIndex].add(word);
     }
 
-    public void displayHashTable(){
-        if(hashTable == null){
+    public void displayHashTable() {
+        if (hashTable == null) {
             System.out.println("EMPTY");
             return;
-        }else{
-            for (int i = 0; i <hashTable.length ; i++) {
+        } else {
+            for (int i = 0; i < hashTable.length; i++) {
                 System.out.println("Index: " + i + ", Key: " + hashTable[i]);
             }
         }
+    }
+
+    public boolean searchHashTable(String word) {
+        int newIndex = modASCIIHashFunction(word, hashTable.length);
+        if (hashTable[newIndex] != null && hashTable[newIndex].contains(word)) {
+            return true;
+        }
+        return false;
+    }
+
+    public void deleteKeyHashTable(String word) {
+        int newIndex = modASCIIHashFunction(word, size);
+        if (searchHashTable(word)) {
+            hashTable[newIndex].remove(word);
+            System.out.println("\"" + word + "\"" + " removed!");
+            return;
+        }
+        System.out.printf(" \"%s\" not found\n", word);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         DirectChaining directChaining = new DirectChaining(15);
-        String[] words = {"Here", "are ", "the", "best", "no-pull", "dog", "harnesses", "you", "can", "buy"};
-        for (int i = 0; i <words.length ; i++) {
+        String[] words = {"Here", "are", "the", "best", "no-pull", "dog", "harnesses", "you", "can", "buy"};
+        for (int i = 0; i < words.length; i++) {
             directChaining.insertHashTable(words[i]);
         }
+        directChaining.displayHashTable();
+        String animal = "horse";
+        System.out.println(directChaining.searchHashTable(animal) ? "The word " + animal + " was found in the table" : "The word "
+                +"\""+ animal+ "\"" + " wasn't found in the table");
+        directChaining.deleteKeyHashTable("dog");
         directChaining.displayHashTable();
     }
 }
