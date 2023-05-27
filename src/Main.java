40b90417e2cca10bc94c87aef32f84082e6748a2
GIL <gil.00000032610@unicap.br>
2023-05-27 10:27:44 -0300

    Preventing collision with Dir.Cha.

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,10 @@
 public class Main {
-
+    public static void main(String[] args) {
+        DirectChaining directChaining = new DirectChaining(15);
+        String[] words = {"Here", "are ", "the", "best", "no-pull", "dog", "harnesses", "you", "can", "buy"};
+        for (int i = 0; i <words.length ; i++) {
+            directChaining.insertHashTable(words[i]);
+        }
+        directChaining.displayHashTable();
+    }
 }
